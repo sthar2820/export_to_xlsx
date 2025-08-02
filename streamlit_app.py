5753e57b5efbfca98f624963fe47fcad0407852f
Rohan Shrestha <103275068+sthar2820@users.noreply.github.com>
2025-08-02 09:45:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -703,7 +703,7 @@ def extract_date(text):
                 dt = datetime.strptime(match, "%m/%Y")
             elif re.match(r"\d{1,2}/\d{2}$", match):  # MM/YY
                 dt = datetime.strptime(match, "%m/%y")
-            elif re.match(r"\d{1,2}/\d{1,2}/\d{4}$", match):  # MM/DD/YYYY
+            elif re.match(r"\d{4}/\d{1,2}/\d{1,2}$", match):  # YYYY/MM/DD
                 dt = datetime.strptime(match, "%m/%d/%Y")
             else:
                 continue
